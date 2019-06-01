196dfde8958e8329cbadd6f0b79c097571074572
AlejandroHerr <ahernandezc@protonmail.com>
2019-06-01 13:16:02 +0200

    feat(MqttClient): Add MqttClientProvider to application

---
+++ src/infrastructure/mqtt/MqttClientProvider.ts
@@ -0,0 +1,21 @@
+import { AsyncClient } from 'async-mqtt';
+import { asFunction } from 'awilix';
+import { Logger } from 'pino';
+
+import { AsyncProvider } from '../Application';
+import { Config } from '../config';
+
+import connectAsyncMqttClient from './mqtt/connectAsyncMqttClient';
+
+const MqttClientProvier: AsyncProvider<AsyncClient> = Object.freeze({
+  name: 'mqttClient',
+  async asyncResolver({ config, logger }: { config: Config; logger: Logger }) {
+    const asyncMqttClient = await connectAsyncMqttClient({ logger, options: config.mqtt });
+
+    return asFunction(() => asyncMqttClient)
+      .singleton()
+      .disposer(mqttClient => mqttClient.end());
+  },
+});
+
+export default MqttClientProvier;
---
+++ src/infrastructure/mqtt/connectAsyncMqttClient.ts
@@ -0,0 +1,44 @@
+import { connect, IClientOptions } from 'mqtt';
+import { AsyncClient } from 'async-mqtt';
+import { Logger } from 'pino';
+
+import asyncSetTimeout from '../../utils/asyncSetTimeout';
+
+const onTimeout = () =>
+  asyncSetTimeout(5000).catch(() => {
+    throw new Error('Mqtt Client connection tiemout');
+  }) as Promise<never>;
+
+const onConnect = (options: IClientOptions) => {
+  const client = connect(options);
+
+  const asyncClient = new AsyncClient(client);
+
+  return new Promise<AsyncClient>((resolve, reject) => {
+    const onError = (error: Error): void => {
+      reject(error);
+    };
+    asyncClient.once('error', onError);
+    asyncClient.once(
+      'connect',
+      (): void => {
+        asyncClient.removeListener('error', onError);
+
+        resolve(asyncClient);
+      },
+    );
+  });
+};
+
+export default ({ logger, options }: { logger: Logger; options: IClientOptions }) =>
+  Promise.race([onConnect(options), onTimeout()])
+    .then(asyncClient => {
+      logger.info(`MQTT client connected to ${options.protocol}://${options.host}:${options.port}`);
+
+      return asyncClient;
+    })
+    .catch(error => {
+      logger.error(error);
+
+      throw error;
+    });
