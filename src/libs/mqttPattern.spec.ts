1146cf96c9ee5dcc02e98fb45cf286a8393ebf5a
AlejandroHerr <ahernandezc@protonmail.com>
2019-06-16 15:19:48 +0200

    test(mqttPattern): Add extra tests for mqttPattern

--- src/libs/mqttPattern.spec.ts
+++ src/libs/mqttPattern.spec.ts
@@ -3,8 +3,21 @@ import originalMqttPattern from 'mqtt-pattern';
 import mqttPattern from './mqttPattern';
 
 describe('lib/mqttPatern', () => {
-  it('should fill only provided values', () => {
-    expect(mqttPattern.fill('+name/#property', { name: 'test' })).toBe('test/#property');
-    expect(originalMqttPattern.fill('+name/#property', { name: 'test' })).toBe('test');
+  describe('fill', () => {
+    it('should fill only provided values', () => {
+      expect(mqttPattern.fill('+name/#property', { name: 'test' })).toBe('test/#property');
+      expect(originalMqttPattern.fill('+name/#property', { name: 'test' })).toBe('test');
+    });
+  });
+  describe('exec', () => {
+    it('should match the pattern', () => {
+      const pattern = '+name/test/#property';
+      expect(mqttPattern.exec(pattern, 'testName/testProp')).toBeNull();
+      expect(mqttPattern.exec(pattern, 'testName/test/testProp')).toEqual({ name: 'testName', property: ['testProp'] });
+      expect(mqttPattern.exec(pattern, 'testName/test/testProp0/testProp1')).toEqual({
+        name: 'testName',
+        property: ['testProp0', 'testProp1'],
+      });
+    });
   });
 });
