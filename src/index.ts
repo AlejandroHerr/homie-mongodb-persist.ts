b1747dfdb52f7a2a2756801f449e3a6525d80c54
AlejandroHerr <ahernandezc@protonmail.com>
2019-06-16 15:58:18 +0200

    fix(index): Fix providers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import Application from './Application';
-import MQTTClientProvider from './providers/MQTTClientProvider';
-import MongoDbClientServiceProvider from './providers/MongoDbClientProvider';
-import MongoDbCollectionsProvider from './providers/MongoDbCollectionsProvider';
 import ConfigProvider from './providers/ConfigProvider';
+import DeviceCollectionProvider from './providers/DeviceCollectionProvider';
+import DeviceFactoryProvider from './providers/DeviceFactoryProvider';
 import LoggerProvider from './providers/LoggerProvider';
+import MongoDbClientServiceProvider from './providers/MongoDbClientProvider';
+import MQTTClientProvider from './providers/MQTTClientProvider';
+import MQTTTopicRouterProvider from './providers/MQTTTopicRouterProvider';
+import NodeCollectionProvider from './providers/NodeCollectionProvider';
+import NodeFactoryProvider from './providers/NodeFactoryProvider';
+import PropertyCollectionProvider from './providers/PropertyCollectionProvider';
+import PropertyFactoryProvider from './providers/PropertyFactoryProvider';
 
 Application.createApplication()
   .register(
     ConfigProvider,
     LoggerProvider,
-    MongoDbClientServiceProvider,
-    MongoDbCollectionsProvider,
     MQTTClientProvider,
+    MQTTTopicRouterProvider,
+    MongoDbClientServiceProvider,
+    DeviceCollectionProvider,
+    NodeCollectionProvider,
+    PropertyCollectionProvider,
+    DeviceFactoryProvider,
+    NodeFactoryProvider,
+    PropertyFactoryProvider,
   )
   .boot()
   .catch(error => {
