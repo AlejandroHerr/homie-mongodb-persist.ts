348c5e518825f9a386c95df4a4462979ec8f153c
AlejandroHerr <ahernandezc@protonmail.com>
2019-06-16 21:38:09 +0200

    feat(Container): Add interface for Container

---
+++ src/libs/Container/index.ts
@@ -0,0 +1,5 @@
+import Container from './Container';
+import { BootableProvider, BootableResolver, Provider, Resolver } from './types';
+
+export default Container;
+export { BootableProvider, BootableResolver, Provider, Resolver };
---
+++ src/libs/Container/types.ts
@@ -0,0 +1,15 @@
+import { Resolver as AwilixResolver } from 'awilix';
+
+export type Resolver<T = any> = AwilixResolver<T>;
+
+export type BootableResolver<T = any> = (args: any) => Promise<Resolver<T>>;
+
+export interface Provider<T = any> {
+  name: string | symbol;
+  resolver: Resolver<T>;
+}
+
+export interface BootableProvider<T = any> {
+  name: string | symbol;
+  bootableResolver: BootableResolver<T>;
+}
