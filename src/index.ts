e4562ed65d112239456d13d9d4041dcd7daee50d
AlejandroHerr <ahernandezc@protonmail.com>
2019-07-07 14:08:57 +0200

    feat(index): Add new services

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { appContainerFactory } from './libs/AppContainer';
 import ConfigProvider from './providers/ConfigProvider';
 import DeviceCollectionProvider from './providers/DeviceCollectionProvider';
 import DeviceFactoryProvider from './providers/DeviceFactoryProvider';
+import DeviceServiceProvider from './providers/DeviceServiceProvider';
+import HomieSubscriptionManagerProvider from './providers/HomieSubscriptionManagerProvider';
 import LoggerProvider from './providers/LoggerProvider';
 import MongoDbClientServiceProvider from './providers/MongoDbClientProvider';
 import MQTTClientProvider from './providers/MQTTClientProvider';
@@ -9,19 +11,25 @@ import MQTTPatternProvider from './providers/MQTTPatternProvider';
 import MQTTRouterProvider from './providers/MQTTRouterProvider';
 import NodeCollectionProvider from './providers/NodeCollectionProvider';
 import NodeFactoryProvider from './providers/NodeFactoryProvider';
+import NodeServiceProvider from './providers/NodeServiceProvider';
 import PropertyCollectionProvider from './providers/PropertyCollectionProvider';
 import PropertyFactoryProvider from './providers/PropertyFactoryProvider';
+import PropertyServiceProvider from './providers/PropertyServiceProvider';
 
 appContainerFactory()
   .register(
     ConfigProvider,
     LoggerProvider,
+    HomieSubscriptionManagerProvider,
     MQTTPatternProvider,
     MQTTRouterProvider,
-    MQTTClientProvider,
     DeviceFactoryProvider,
     NodeFactoryProvider,
     PropertyFactoryProvider,
+    DeviceServiceProvider,
+    NodeServiceProvider,
+    PropertyServiceProvider,
+    MQTTClientProvider,
     MongoDbClientServiceProvider,
     DeviceCollectionProvider,
     NodeCollectionProvider,
